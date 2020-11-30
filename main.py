1e8d81e83881b4ecdd8ea52261b5af8b0d5b907c
CC WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2020-11-30 21:43:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1421385508:AAH8ImgpqlxuK57-9hEm6kX_jglRcoSRoWY"
 
 def download(bot, update):
     message = update.effective_message
